86ae6e16e37ec61b1cdcd97af616af4afb6bdc39
Munawar Alam <munawaralamuk@aol.com>
2025-05-01 00:09:05 +0100

    update project new files

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -54,6 +54,44 @@ resource "aws_iam_role_policy_attachment" "bucket_policy_attachment" {
   policy_arn = aws_iam_policy.bucket_policy.arn
 }
 
+resource "aws_iam_role" "iam_for_sfn" {
+  name_prefix        = "role-for-sfn-"
+  assume_role_policy = <<EOF
+{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "states.amazonaws.com",
+                        "events.amazonaws.com",
+                        "scheduler.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    }
+EOF
+}
 
-
-
+resource "aws_iam_role" "eventbridge_scheduler_iam_role" {
+  name_prefix         = "eventbridge-scheduler-role-"
+  assume_role_policy  = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "scheduler.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
