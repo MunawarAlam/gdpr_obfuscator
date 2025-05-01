7f9bff1a289bbe161429ae1d2e52d968bb5cfa1a
Munawar Alam <munawaralamuk@aol.com>
2025-05-01 04:55:08 +0100

    update project new files

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -53,6 +53,29 @@ resource "aws_iam_role_policy_attachment" "bucket_policy_attachment" {
   role = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.bucket_policy.arn
 }
+# CREATING AND ATTACHING CLOUDWATCH POLICES
+data "aws_iam_policy_document" "cloudwatch_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = ["logs:CreateLogGroup"]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["*"]
+}
+}
+
+resource "aws_iam_policy" "cloudwatch_policy" {
+  name = "lambda_cloudwatch_policy"
+  policy = data.aws_iam_policy_document.cloudwatch_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.cloudwatch_policy.arn
+}
 
 resource "aws_iam_role" "iam_for_sfn" {
   name_prefix        = "role-for-sfn-"
@@ -78,6 +101,34 @@ resource "aws_iam_role" "iam_for_sfn" {
 EOF
 }
 
+
+resource "aws_iam_policy_attachment" "sfn_lambda_execution" {
+  name       = "sfn_lambda_execution_attachment"
+  roles      = [aws_iam_role.iam_for_sfn.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+}
+
+resource "aws_iam_policy" "eventbridge_access_policy" {
+    name = "eventbridge-access-policy"
+
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "states:StartExecution"
+            ],
+            "Resource": [
+                "${aws_sfn_state_machine.sfn_state_machine.arn}"
+            ]
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role" "eventbridge_scheduler_iam_role" {
   name_prefix         = "eventbridge-scheduler-role-"
   assume_role_policy  = <<EOF
@@ -94,4 +145,41 @@ resource "aws_iam_role" "eventbridge_scheduler_iam_role" {
     ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler" {
+  policy_arn = aws_iam_policy.eventbridge_access_policy.arn
+  role       = aws_iam_role.eventbridge_scheduler_iam_role.name
+}
+##
+resource "aws_iam_role_policy" "sns_publish_policy" {
+  name = "sns-publish"
+  role = aws_iam_role.lambda_role.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:StartQuery",
+          "logs:GetQueryResults",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:ListAccountAliases",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
