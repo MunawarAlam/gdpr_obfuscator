194e782222f19730ad961f311589ff35da17c22d
Munawar Alam <munawaralamuk@aol.com>
2025-05-02 06:46:00 +0100

    terafrom

---
+++ terraform/stepfunctions.tf
@@ -0,0 +1,36 @@
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "ma-gdpr-state-machine"
+  role_arn = aws_iam_role.iam_for_sfn.arn
+
+  definition = <<EOF
+{
+  "Comment": "Extract Data using an AWS Lambda Function",
+  "StartAt": "ingestion lambda",
+  "States": {
+    "ingestion lambda": {
+      "Type": "Task",
+      "Resource": "${aws_lambda_function.ingestion.arn}",
+      "Parameters": {
+        "Payload.$": "$",
+        "FunctionName": "${aws_lambda_function.ingestion.arn}"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 3,
+          "BackoffRate": 2,
+          "JitterStrategy": "FULL"
+        }
+      ],
+      "End": true
+    }
+  }
+}
+EOF
+}
