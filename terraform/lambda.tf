382bf16da1789d995a9001f15f76fa0db6d99f91
Munawar Alam <munawaralamuk@aol.com>
2025-05-06 23:36:34 +0100

    updating aws lambda functions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,6 +5,21 @@ data "archive_file" "ingestion" {
   output_path = "${path.module}/../src/ingestion.zip"
 }
 
+# data "archive_file" "pandas_layer" {
+#   type = "zip"
+#   output_file_mode = "0666"
+#   source_dir = "${path.module}/../layer"
+#   output_path = "${path.module}/../layer.zip"
+# }
+
+resource "aws_lambda_layer_version" "pandas_layer" {
+  layer_name = "pandas_layer"
+  compatible_runtimes = ["python3.12"]
+  s3_bucket = aws_s3_bucket.lambda_code_bucket.id
+  s3_key = aws_s3_object.pandas_layer.key
+}
+
+
 resource "aws_lambda_function" "ingestion" {
   function_name = "ingestion"
   handler = "ingestion.lambda_handler"
@@ -13,6 +28,7 @@ resource "aws_lambda_function" "ingestion" {
   s3_bucket = aws_s3_bucket.lambda_code_bucket.id
   s3_key = aws_s3_object.ingestion_lambda.key
   role = aws_iam_role.lambda_role.arn
+  layers = ["arn:aws:lambda:eu-north-1:336392948345:layer:AWSSDKPandas-Python312:16"]
   memory_size = 500
   environment {
     variables = {
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -14,3 +14,9 @@ resource "aws_s3_object" "ingestion_lambda" {
   key = "write_to_s3_lambda"
   source = "${path.module}/../src/ingestion.zip"
 }
+
+resource "aws_s3_object" "pandas_layer" {
+  bucket = aws_s3_bucket.lambda_code_bucket.id
+  key = "pandas_layer"
+  source = "${path.module}/../layer.zip"
+}
